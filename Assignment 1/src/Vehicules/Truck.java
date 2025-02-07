c7efc5d95767971d4323e0deebbfb034a7ca745a
mountagv7 <tagasy05@gmail.com>
2025-02-07 14:54:47 -0500

    truck constructor

--- Assignment 1/src/Vehicules/Truck.java
+++ Assignment 1/src/Vehicules/Truck.java
@@ -3,13 +3,13 @@ package Vehicules;
 public class Truck extends Vehicule {
 private double maxWeight;
 private static int vehiculeNumber = 1000;
-private String plateNumber;
+
 
 public Truck(){
     super();
     this.maxWeight=0.0;
     vehiculeNumber++;
-    this.plateNumber=
+   
 }
 public Truck(int make, String model, int year, double maxWeight){
     super(make, model, year);
